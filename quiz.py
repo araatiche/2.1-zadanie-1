4af1a02e9e4b0df8121df469cf249f8d447cac10
araatiche <araatiche@yandex.ru>
2020-12-18 23:52:26 +0300

    start task

---
+++ quiz.py
@@ -0,0 +1,31 @@
+import sqlite3
+from sqlite3 import Error
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+end_message = " хуйня"
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    if request.method == 'GET':
+        return render_template('quiz.html')
+    else:
+        user_answer_1 = request.form["Question1"]
+        user_answer_2 = request.form["Question2"]
+        user_answer_3 = request.form["Question3"]
+        print(user_answer_1,user_answer_2,user_answer_3)
+
+        return render_template('summary.html', end_message="ЖОПА!!!")
+    
+
+@app.route('/summary', methods=['GET', 'POST'])
+def summary():
+    
+    return render_template('summary.html', end_message=end_message)
+
+
+def main():
+    # execute_query(connection, create_user_table)
+    app.run()
+main()
